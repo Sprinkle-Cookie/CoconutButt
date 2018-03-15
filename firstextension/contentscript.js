9e0d14cacc368b5c3645bc59422355233b2e238a
Nandaja <nandaja.varma@gmail.com>
2018-03-15 20:47:03 +0100

    And red for all the sites

--- firstextension/contentscript.js
+++ firstextension/contentscript.js
@@ -11,17 +11,41 @@ function chooseResponse(response){
 		highlight(response);
 	}
 }
+function getTextTag(){
+        var tag;
+        tag =  document.getElementById('mw-content-text');
+        if(tag == null){
+                    tag = document.getElementsByTagName("body")[0];
+                }
+        return tag;
+}
 
 function getText(message){
-	var text = document.getElementById('mw-content-text').textContent;
-    browser.runtime.sendMessage({"text": text });
-    console.log("sent text to background") ;
+
+        var tag = getTextTag();
+        var text = "";
+
+        Array.from(tag.getElementsByTagName("p")).forEach(function(elem){
+                    text += elem.textContent + " ";
+                });
+
+        console.log('text is', text);
+        console.log('lang is', document.documentElement.lang);
+        browser.runtime.sendMessage({"lang": document.documentElement.lang, "text": text });
+        console.log("sent text to background") ;
 }
+
+//function getText(message){
+	//var text = document.getElementById('mw-content-text').textContent;
+    //browser.runtime.sendMessage({"text": text });
+    //console.log("sent text to background") ;
+//}
 
 function highlight(words){
 	//$('#mw-content-text').mark(words, {'accuracy':'exactly', "diacritics": false});
+    var tag = getTextTag();
     for (var i = 0; i < words.length; i++) {
-        Array.from(document.getElementById('mw-content-text').getElementsByTagName('p')).forEach(function(elem) {
+        Array.from(tag.getElementsByTagName('p')).forEach(function(elem) {
 			highlight2(elem, words[i]);
         });
     }
@@ -29,8 +53,18 @@ function highlight(words){
 
 function highlight2(container,what) {
     var content = container.innerHTML,
-        pattern = new RegExp('(>[^<.]*)(\\b' + what + '\\b)([^<.]*)','g'),
-        replaceWith = '$1<span style="color:red;"' + '">$2</span>$3',
-        highlighted = content.replace(pattern,replaceWith);
-    return (container.innerHTML = highlighted) !== content;
+        newcn = container.innerHTML,
+        pattern = new RegExp('^[^<]{3,}|>[^<]{3,}[^<]','g'),
+        replace_regex = new RegExp('(\\b' + what + '\\b)', 'ig'),
+        replaceWith = '<span style="color:red;">$1</span>',
+        match = pattern.exec(content);
+        while(match != null){
+            text_block1 = match[0];
+            text_block2 = text_block1.replace(replace_regex,replaceWith);
+
+            newcn = newcn.replace(text_block1, text_block2);
+            match = pattern.exec(content);
+        }
+
+    return (container.innerHTML = newcn) !== content;
 }
